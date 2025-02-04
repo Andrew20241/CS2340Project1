46352a1fcd9e90c74f50bb4d9c2f72f30abb707d
benrez <119080501+benrez@users.noreply.github.com>
2025-02-04 17:56:36 -0500

    Added image view to database

--- GTMovies/movies/models.py
+++ GTMovies/movies/models.py
@@ -5,6 +5,6 @@ class Movie(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    #image = models.ImageField(upload_to='movie_images/')
+    image = models.ImageField(upload_to='movie_images/')
     def __str__(self):
         return str(self.id) + ' - ' + self.name
