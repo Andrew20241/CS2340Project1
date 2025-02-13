f34c4cd0b20ba3864b87d2daac87d3de6e2b765e
zshamsi3 <zshamsi3@gatech.edu>
2025-02-13 13:49:18 -0500

    update csrf cors

--- GTMovies/GTMovies/settings.py
+++ GTMovies/GTMovies/settings.py
@@ -27,13 +27,19 @@ SECRET_KEY = 'django-insecure-k!p=%v)d4_!6bc3ou1bdwkv#fbggxf$ev79w#6!8x2)1_z$9=-
 DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://cs2340-gtmovies-1cd7e7abd44d.herokuapp.com/'
-],
+    'https://cs2340-gtmovies-1cd7e7abd44d.herokuapp.com',
+    "https://localhost",
+    "http://localhost",
+]
+
 ALLOWED_HOSTS = [
     '.herokuapp.com',
-],
+]
+
 CORS_ORIGIN_WHITELIST = [
-    'https://cs2340-gtmovies-1cd7e7abd44d.herokuapp.com/',
+    'https://cs2340-gtmovies-1cd7e7abd44d.herokuapp.com',
+    "https://localhost",
+    "http://localhost",
 ]
 
 # Application definition
