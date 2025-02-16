e064ab5054d2e5a819b2333ad7317b4c72af1af8
Dylan Keyser <dylankeyser1@gmail.com>
2025-02-15 21:09:29 -0500

    Added email sending and password resetting functionality

--- GTMovies/GTMovies/settings.py
+++ GTMovies/GTMovies/settings.py
@@ -149,3 +149,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'GTMovies/static/',
 ]
+
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dylankeyser1@gmail.com'
+EMAIL_HOST_PASSWORD = 'lmxy tffn khvr khfb'
--- GTMovies/accounts/views.py
+++ GTMovies/accounts/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import login as auth_login, authenticate, logout as aut
 from django.shortcuts import redirect
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
@@ -52,3 +55,13 @@ def orders(request):
     template_data['title'] = 'Orders'
     template_data['orders'] = request.user.order_set.all()
     return render(request, 'accounts/orders.html', {'template_data': template_data})
+
+class ResetPassword(SuccessMessageMixin, PasswordResetView):
+    template_name = 'accounts/password_reset.html'
+    email_template_name = 'accounts/password_reset_email.html'
+    subject_template_name = 'accounts/password_reset_subject.txt'
+    success_message = "We've emailed you instructions for setting your password " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('home.index')
